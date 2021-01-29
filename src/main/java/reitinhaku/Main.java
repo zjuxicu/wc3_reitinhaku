9ef282a680c8585aec5ed7d20a2babd654661279
Tatu <tatu@castren.com>
2021-01-29 14:47:04 +0200

    viikko2 raportti

--- src/main/java/reitinhaku/Main.java
+++ src/main/java/reitinhaku/Main.java
@@ -12,7 +12,6 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-
         Kayttoliittyma kl = new Kayttoliittyma();
 
     }
